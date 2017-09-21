69892deceb546628c9c5abf7264e4a365ac90dc3
Daniele Di Minica <d.diminica@resi.it>
2017-09-21 16:11:21 +0200

    Aggiunte ricerca e paginazione

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -ti --rm --name animals -v `pwd`:/srv/jekyll -p 4000:4000 jekyll/jekyll
+
+docker run -ti --rm --name animals -v `pwd`:/srv/jekyll -p 4000:4000 jekyll/jekyll jekyll serve
